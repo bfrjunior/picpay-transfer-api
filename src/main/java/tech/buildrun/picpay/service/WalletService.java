71be885ba895cf666a7db6b8cd65296b35d258c9
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-10 01:34:21 -0300

    feat:exception error

--- src/main/java/tech/buildrun/picpay/service/WalletService.java
+++ src/main/java/tech/buildrun/picpay/service/WalletService.java
@@ -17,6 +17,10 @@ public class WalletService {
     public Wallet createWallet(CreateWalletDto dto) {
 
         var walletDb = walletRepository.findByCpfCnpjOrEmail(dto.cpfCnpj(), dto.email());
+        if (walletDb.isPresent()) {
+            throw new WalletDataAlreadyExistsException("CpfCnpj or Email already exists");
+        }
+
         return walletRepository.save(dto.toWallet());
     }
 
