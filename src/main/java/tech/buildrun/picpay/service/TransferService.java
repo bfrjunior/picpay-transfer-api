a5edbed5e71dc571803b3f7a7174fbe4acb43127
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-10 02:50:24 -0300

    feat: transferencia

---
+++ src/main/java/tech/buildrun/picpay/service/TransferService.java
@@ -0,0 +1,15 @@
+package tech.buildrun.picpay.service;
+
+import org.springframework.stereotype.Service;
+import tech.buildrun.picpay.entity.Transfer;
+import tech.buildrun.picpay.controller.dto.TransferDto;
+
+@Service
+public class TransferService {
+
+    public Transfer transfer(TransferDto transferDto) {
+
+        return null;
+    }
+
+}
