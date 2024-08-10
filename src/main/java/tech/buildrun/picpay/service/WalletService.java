ba1679a651ca833526e6aa0f136c6cf489f0b975
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-10 01:49:16 -0300

    feat: Rest Exception

--- src/main/java/tech/buildrun/picpay/service/WalletService.java
+++ src/main/java/tech/buildrun/picpay/service/WalletService.java
@@ -3,6 +3,7 @@ package tech.buildrun.picpay.service;
 import org.springframework.stereotype.Service;
 import tech.buildrun.picpay.controller.dto.CreateWalletDto;
 import tech.buildrun.picpay.entity.Wallet;
+import tech.buildrun.picpay.exception.WalletDataAlreadyExistsException;
 import tech.buildrun.picpay.repository.WalletRepository;
 
 @Service
